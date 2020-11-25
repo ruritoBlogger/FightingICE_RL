6d4642550e17228a35bdd9402be0e0d2fd77a2c4
rurito0125 <rurito.blog@gmail.com>
2020-11-25 20:00:26 +0900

    feat: 各機能を組み込んで試合を実行出来るようにした

--- FTG4.50/trainer/trainer.py
+++ FTG4.50/trainer/trainer.py
@@ -1,15 +1,19 @@
 import gym
 from gym_fightingice.envs.Machete import Machete
+from observer import Observer
+from rolebaseAgent import RoleBaseAgent
 
 def main():
     env = gym.make("FightingiceDataNoFrameskip-v0", java_env_path="/home/troll/lesson/ken/FTG4.50")
 
     frame_data = env.reset(p2="MctsAi")
+    observer = Observer()
+    agent = RoleBaseAgent()
     done = False
     
     while not done:
-        if frame_data[67] >= 300 and frame_data[0] - frame_data[66] <= 300:
-            frame_data, reward, done, info = env.step(32)
+        action = agent.get_action(observer.transform(frame_data))
+        frame_data, reward, done, info = env.step(action)
 
 
 if __name__ == "__main__":
