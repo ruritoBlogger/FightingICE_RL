45655a39ddf9033a393bc186edd128991361b371
ruritoBlogger <rurito.blog@gmail.com>
2020-12-03 04:29:00 +0900

    feat: observerの仕様を変えたのでそれに合わせて修正

--- FTG4.50/trainer/trainer.py
+++ FTG4.50/trainer/trainer.py
@@ -5,16 +5,15 @@ from rolebaseAgent import RoleBaseAgent
 
 def main():
     env = gym.make("FightingiceDataNoFrameskip-v0", java_env_path="/home/rurito/lesson/ken/FTG4.50")
-
-    frame_data = env.reset(p2="MctsAi")
-    observer = Observer()
+    p2 = "MctsAi"
+    env = Observer(env, p2)
+    frame_data = env.reset()
     agent = RoleBaseAgent()
     done = False
     
     while not done:
-        action = agent.get_action(observer.transform(frame_data))
+        action = agent.get_action(frame_data)
         frame_data, reward, done, info = env.step(action)
-
 
 if __name__ == "__main__":
     main()
